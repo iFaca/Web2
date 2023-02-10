910c5c843fc448a5a6bc5278d2b4eeceabe66270
Ezequiel Montero <ezequielmonterodev@gmail.com>
2023-02-10 13:27:01 -0300

    subo los componentes funcionales de toda la web

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Main from '../src/Components/Main/Main'
+import Services from './Components/Services/Services';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
 
 
 function App() {
   return (
     <div className="App">
       <Main/>
+      <Services/>
+      <About/>
+      <Contact/>
     </div>
   );
 }
