a1f1a3d303f0e4c804a17bbc42b7f162e3d4731d
Ezequiel Montero <ezequielmonterodev@gmail.com>
2023-02-10 14:13:29 -0300

    Agrego imagenes y el hamburguer menu

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main from '../src/Components/Main/Main'
 import Services from './Components/Services/Services';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,11 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import "./Main.css";
+import Logo from "../../Assets/Img/innovis_adobe_express.png";
+import Navbar from "../Nav-bar/Navbar";
 const Main = () => {
-    return (
-        <div>
-            <h1>MAIN</h1>
-        </div>
-    );
-}
+  return (
+    <div className="head">
+      <div className="nav-cont">
+        <img src={Logo} alt="logo"></img>
+        <p className="slogan">El futuro de tu empresa esta acá</p>
+        <Navbar/>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
