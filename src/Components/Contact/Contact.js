c22be69c7406d54b6894215262155d47eafaf8b0
Ezequiel Montero <ezequielmonterodev@gmail.com>
2023-02-13 17:06:54 -0300

    maquetado total de la web terminado en 320px

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,44 @@
-import React from 'react';
-
+import React from "react";
+import "./Contact.css";
+import Geotop from '../../Assets/Img/geometria2.png'
+import Geobot from '../../Assets/Img/geometria.png'
 const Contact = () => {
-    return (
-        <div>
-            <h1>Contact</h1>
-        </div>
-    );
-}
+  return (
+    <div className="contact">
+     <img src={Geotop} className="geo-contact"/>
+      <h2>Contacto</h2>
+      <h3>
+        Contactenos por whatsapp, zoom o una llamada telefónica.
+        <spam className="highlight">
+          Programe ahora una reunión con nuestros asistentes.
+        </spam>
+      </h3>
+
+      <div className="form-cont">
+        <input placeholder="Nombre"></input>
+        <div className="line"></div>
+        <input placeholder="Email"></input>
+        <div className="line"></div>
+        <input placeholder="Asunto"></input>
+        <div className="line"></div>
+        <textarea
+          id=""
+          name=""
+          rows="8"
+          cols="30"
+          placeholder="deja tu consulta..."
+        ></textarea>
+        <button className="btn-app">Quiero innovar</button>
+      </div>
+      <h2>DATOS DE CONTACTO</h2>
+      <h3>Viamonte 5263,Chacras de Coria Luján de Cuyo,Mendoza.</h3>
+      <h3>Hola@innovis.com.ar</h3>
+      <h3>+54 9 261 516 3469</h3>
+      <h3>+54 9 261 622 5439</h3>     
+      <h3>+54 9 261 513 2968</h3>     
+    <img src={Geobot} className="geo-contact-bot"/>
+    </div>
+  );
+};
 
 export default Contact;
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import './Services.css'
+import Geo from '../../Assets/Img/geometria.png'
 
 const Services = () => {
-    return (
-        <div>
-            <h1>services</h1>
-        </div>
-    );
-}
+  return (
+    <><div className="services">
+          <h2>SOFTWARE</h2>
+          <p className="services-info">
+              Soluciones enlatadas y a medida para cualquier tipo de empresa.Vos hace
+              crecer tu negocio que nosotros nos encargamos de los sistemas.
+          </p>
+          <h2>WEB 3.0</h2>
+          <p className="services-info">
+              Innovamos de forma permanente y sabemos que la web 3.0 es lo que se
+              viene.Consutanos por las soluciones en blockchain y llega mas rápido al
+              futuro lector.
+          </p>
+          <h2>SOPORTE</h2>
+          <p className="services-info">
+              Brindamos soporte,capacitación y asesoramiento IT a cualquier tipo de
+              industria,empresa u organizacion.Nos especializamos en brindar
+              soluciones.
+          </p>
+          
+      </div>
+      <img src={Geo} className='geo'></img>
+      <img src={Geo} className='geo2'></img>
+
+      </>
+  );
+};
 
 export default Services;
