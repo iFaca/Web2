e8d772beda2ad0dd9dce079db4bb85d2147284c5
Ezequiel Montero <ezequielmonterodev@gmail.com>
2023-02-22 13:30:58 -0300

    cambios en responsive del componente services

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,29 +1,36 @@
 import React from "react";
-import './Services.css'
-import Geo from '../../Assets/Img/geometria.png'
+import "./Services.css";
+import Geo from "../../Assets/Img/geometria.png";
 
 const Services = () => {
   return (
     <>
       <div className="services">
         <div className="services-content">
-          <h2>SOFTWARE</h2>
-          <p className="services-info">
-            Soluciones enlatadas y a medida para cualquier tipo de empresa.Vos
-            hace crecer tu negocio que nosotros nos encargamos de los sistemas.
-          </p>
-          <h2>WEB 3.0</h2>
-          <p className="services-info">
-            Innovamos de forma permanente y sabemos que la web 3.0 es lo que se
-            viene.Consutanos por las soluciones en blockchain y llega mas rápido
-            al futuro lector.
-          </p>
-          <h2>SOPORTE</h2>
-          <p className="services-info">
-            Brindamos soporte,capacitación y asesoramiento IT a cualquier tipo
-            de industria,empresa u organizacion.Nos especializamos en brindar
-            soluciones.
-          </p>
+          <div className="services-obj">
+            <h2>SOFTWARE</h2>
+            <p className="services-info">
+              Soluciones enlatadas y a medida para cualquier tipo de empresa.Vos
+              hace crecer tu negocio que nosotros nos encargamos de los
+              sistemas.
+            </p>
+          </div>
+          <div className="services-obj">
+            <h2>WEB 3.0</h2>
+            <p className="services-info">
+              Innovamos de forma permanente y sabemos que la web 3.0 es lo que
+              se viene.Consutanos por las soluciones en blockchain y llega mas
+              rápido al futuro lector.
+            </p>
+          </div>
+          <div className="services-obj">
+            <h2>SOPORTE</h2>
+            <p className="services-info">
+              Brindamos soporte,capacitación y asesoramiento IT a cualquier tipo
+              de industria,empresa u organizacion.Nos especializamos en brindar
+              soluciones.
+            </p>
+          </div>
         </div>
       </div>
       <img src={Geo} className="geo"></img>
