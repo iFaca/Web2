173a19d1e9ca971126f8798e406f123fd25e9e9a
Ezequiel Montero <ezequielmonterodev@gmail.com>
2023-03-01 13:38:05 -0300

    subo cambios en el formulario y validaciones

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,26 +1,64 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from '@emailjs/browser';
 import "./Contact.css";
 import Geotop from "../../Assets/Img/geometria2.png";
 import Geobot from "../../Assets/Img/geometria.png";
+
 const Contact = () => {
-  
-  
   const form = useRef();
 
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [subjectError, setSubjectError] = useState("");
+  
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_3on12bj', 'template_qlpb9is', form.current, 'Ek8vPmaGvSCU7nbM4')
+    let valid = true;
+    
+    // Validación del campo nombre
+    if (!form.current.user_name.value.trim()) {
+      setNameError("Por favor ingrese su nombre");
+      valid = false;
+    } else if (!/^[a-zA-Z\s]+$/.test(form.current.user_name.value.trim())) {
+      setNameError("Por favor ingrese un nombre válido sin números");
+      valid = false;
+    } else {
+      setNameError("");
+    }
+
+    // Validación del campo email
+    if (!form.current.user_email.value.trim()) {
+      setEmailError("Por favor ingrese su email");
+      valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(form.current.user_email.value.trim())) {
+      setEmailError("Por favor ingrese un email válido");
+      valid = false;
+    } else {
+      setEmailError("");
+    }
+
+    // Validación del campo asunto
+    if (!form.current.subject.value.trim()) {
+      setSubjectError("Por favor ingrese el asunto");
+      valid = false;
+    } else if (!/^[a-zA-Z\s]+$/.test(form.current.subject.value.trim())) {
+      setSubjectError("Por favor ingrese un asunto válido sin números");
+      valid = false;
+    } else {
+      setSubjectError("");
+    }
+
+    // Si todos los campos son válidos, se envía el formulario
+    if (valid) {
+      emailjs.sendForm('service_3on12bj', 'template_qlpb9is', form.current, 'Ek8vPmaGvSCU7nbM4')
       .then((result) => {
-          alert('email enviado con exito')
+        alert('email enviado con exito')
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
+    }
   };
-  
-  
-  
   return (
     <>
       <div className="contact-title">
@@ -36,22 +74,25 @@ const Contact = () => {
             </spam>
           </h3>
           <form ref={form} onSubmit={sendEmail} className="field">
-          <div className="form-cont">
-            <input placeholder="Nombre" name="user_name"></input>
-            <div className="line"></div>
-            <input placeholder="Email" name="user_email"></input>
-            <div className="line"></div>
-            <input placeholder="Asunto"></input>
-            <div className="line"></div>
-            <textarea
-              name="message"
-              id=""
-              rows="8"
-              cols="10"
-              placeholder="Detalle del proyecto(opcional)"
-            ></textarea>
-            <button className="btn-app">QUIERO INNOVAR</button>
-          </div>
+            <div className="form-cont">
+              <input placeholder="Nombre" name="user_name"></input>
+              {nameError && <p className="error-message">{nameError}</p>}
+              <div className="line"></div>
+              <input placeholder="Email" name="user_email"></input>
+              {emailError && <p className="error-message">{emailError}</p>}
+              <div className="line"></div>
+              <input placeholder="Asunto" name="user_subject"></input>
+              {subjectError && <p className="error-message">{subjectError}</p>}
+              <div className="line"></div>
+              <textarea
+                name="message"
+                id=""
+                rows="8"
+                cols="10"
+                placeholder="Detalle del proyecto(opcional)"
+              ></textarea>
+              <button className="btn-app">QUIERO INNOVAR</button>
+            </div>
           </form>
         </div>{" "}
         <img src={Geobot} className="geo-contact-bot" alt=""/>
