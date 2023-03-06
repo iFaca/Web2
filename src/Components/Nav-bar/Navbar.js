e24b325cf02623604d8f70a345def9219fe21182
Ezequiel Montero <ezequielmonterodev@gmail.com>
2023-03-06 18:29:31 -0300

    agrego funcionalidad de desplazamiento desde el navbar

--- src/Components/Nav-bar/Navbar.js
+++ src/Components/Nav-bar/Navbar.js
@@ -1,23 +1,82 @@
 import React from "react";
-import './Navbar.css';
-
+import { Link } from "react-scroll";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="hamburger-menu">
-    <input id="menu__toggle" type="checkbox" />
-    <label className="menu__btn" for="menu__toggle">
-      <span></span>
-    </label>
+      <input id="menu__toggle" type="checkbox" />
+      <label className="menu__btn" htmlFor="menu__toggle">
+        <span></span>
+      </label>
 
-    <ul className="menu__box">
-      <li><a className="menu__item" href="#">Inicio</a></li>
-      <li><a className="menu__item" href="#">Servicios</a></li>
-      <li><a className="menu__item" href="#">Nosotros</a></li>
-      <li><a className="menu__item" href="#">Contacto</a></li>
-    </ul>
-
-  </div>
+      <ul className="menu__box">
+        <li>
+          <Link
+            activeClass="active"
+            to="main"
+            spy={true}
+            smooth={true}
+            offset={-80}
+            className="menu__item"
+            duration={500}
+            onClick={() => {
+              document.getElementById("menu__toggle").checked = false;
+            }}
+          >
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="services"
+            className="menu__item"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            onClick={() => {
+              document.getElementById("menu__toggle").checked = false;
+            }}
+          >
+            Servicios
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="about"
+            className="menu__item"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            onClick={() => {
+              document.getElementById("menu__toggle").checked = false;
+            }}
+          >
+            Nosotros
+          </Link>
+        </li>
+        <li>
+          <Link
+            activeClass="active"
+            to="contact"
+            className="menu__item"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            onClick={() => {
+              document.getElementById("menu__toggle").checked = false;
+            }}
+          >
+            Contacto
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 
